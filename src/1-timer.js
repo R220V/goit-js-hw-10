cd1638af6b9c327e50c59063bd3c4303b1e3e789
Krus <krus.margo@gmail.com>
2025-01-11 19:08:45 +0200

    error

--- src/1-timer.js
+++ src/1-timer.js
@@ -21,24 +21,33 @@ iziToast.show({
   message: 'What would you like to add?',
 });
 
-co /
-  // function convertMs(ms) {
-  //   // Number of milliseconds per unit of time
-  //   const second = 1000;
-  //   const minute = second * 60;
-  //   const hour = minute * 60;
-  //   const day = hour * 24;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
-  //   // Remaining days
-  //   const days = Math.floor(ms / day);
-  //   // Remaining hours
-  //   const hours = Math.floor((ms % day) / hour);
-  //   // Remaining minutes
-  //   const minutes = Math.floor(((ms % day) % hour) / minute);
-  //   // Remaining seconds
-  //   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  //   return { days, hours, minutes, seconds };
-  // }
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  flatpickr(refs.datetimePicker, options);
+//   return { days, hours, minutes, seconds };
+// }
+
+flatpickr(refs.datetimePicker, options);
