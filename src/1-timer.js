9848a1ce8ec77c6c28b426af5def85c6881755de
Krus <krus.margo@gmail.com>
2025-01-11 20:47:17 +0200

    error

--- src/1-timer.js
+++ src/1-timer.js
@@ -4,34 +4,44 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-// let userSelectedDate = null;
-// let countdownInterval = null;
+let userSelectedDate = null;
+let countdownInterval = null;
 
-// const refs = {
-//   datetimePicker: document.querySelector('#datetime-picker'),
-//   startButton: document.querySelector('[data-start]'),
-//   daysValue: document.querySelector('[data-days]'),
-//   hoursValue: document.querySelector('[data-hours]'),
-//   minutesValue: document.querySelector('[data-minutes]'),
-//   secondsValue: document.querySelector('[data-seconds]'),
-// };
+const refs = {
+  datetimePicker: document.querySelector('#datetime-picker'),
+  startButton: document.querySelector('[data-start]'),
+  daysValue: document.querySelector('[data-days]'),
+  hoursValue: document.querySelector('[data-hours]'),
+  minutesValue: document.querySelector('[data-minutes]'),
+  secondsValue: document.querySelector('[data-seconds]'),
+};
 
 iziToast.show({
   title: 'Hey',
   message: 'What would you like to add?',
 });
 
-flatpickr(refs.datetimePicker, options);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+      refs.startButton.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      refs.startButton.disabled = false;
+    }
+  },
+};
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
+flatpickr(refs.datetimePicker, options);
 
 // function convertMs(ms) {
 //   // Number of milliseconds per unit of time
